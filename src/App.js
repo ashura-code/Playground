f8fe358d93c4dec9e1de7282de4187d4f951e220
ashura-code <83760410+ashura-code@users.noreply.github.com>
2022-10-03 16:31:50 +0530

    Learned until single multipages links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import {Route,Routes} from 'react-router-dom';
 import About from  './about';
-import Home from './home';
+import Home from './home'; 
 
 
 function App() {
   return (
     <div>
+      <nav><h2>Dont mind me, im the nav btw..</h2></nav>
     <Routes>
 
-   <Route path="/" element={<Home/>}/>
-   <Route path="/about" element={<About/>}/>
+   <Route path="/wow" element={<Home/>}>
+     <Route path="abou" element={<div> <h1>This is rendered fpfv dfknvdv</h1></div>}></Route>
+   </Route>
+
+   
+    <Route path="/about" element={<About/>}/>
+
+    <Route path="*" element={<center><h1>There may be an typo in your address,,please check it </h1></center>}/>
 
 
+
+   
+   
 
     </Routes>
 
--- src/home.js
+++ src/home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link,Outlet} from 'react-router-dom';
 
 const Home = () => {
     return ( 
         <div>
                 <h1>This is the home page</h1>
                 <Link to="about">visit our about page</Link>
+                <Outlet/>
 
         </div>
      );
